40bea8029e9b5672ea5c5eac8d519616bdaf71d2
Walber Rocha <walber_jesus@hotmail.com>
2020-11-30 16:31:56 -0300

    Update
    Change certificate

--- src/backend/src/index.js
+++ src/backend/src/index.js
@@ -4,24 +4,23 @@ const mongoose = require('mongoose');
 const routes = require('./routes');
 const path = require('path');
 const fs = require('fs');
-const tls = require('tls');
 
-var privateKey = fs.readFileSync('privatekey.pem').toString();
-var certificate = fs.readFileSync('certificate.pem').toString();
-
-var credentials = tls.createSecureContext({ key: privateKey, cert: certificate });
+const credentials = {
+    key: fs.readFileSync('key.pem'),
+    cert: fs.readFileSync('cert.pem')
+};
 
 const twittaDEV = express();
 
 const parser = require('body-parser');
 const urlencodedParser = parser.urlencoded({extended : false});
 
-const server = require('http').Server(credentials, twittaDEV);
+const server = require('https').createServer(credentials, twittaDEV);
 const io = require('socket.io')(server);
 
 const url = "mongodb+srv://root:root@twittadev.58xby.mongodb.net/twittaDEV?retryWrites=true&w=majority";
 
-mongoose.connect( url, { 
+mongoose.connect( url, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
